424fedfc1e0b694d48a79b6780558d5471439772
zhouyongfa <fightzyf1992@hotmail.com>
2018-06-26 16:36:41 +0800

    constant pool and member info

--- ch03/classfile/class_file.go
+++ ch03/classfile/class_file.go
@@ -3,6 +3,7 @@ package classfile
 import "fmt"
 
 type ClassFile struct {
+	magic				uint32
 	minorVersion		uint16
 	majorVersion 		uint16
 	constantPool		ConstantPool
@@ -16,19 +17,44 @@ type ClassFile struct {
 }
 
 func Parse(classData [] byte) (cf *ClassFile, err error) {
-
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("%v", range)
+			}
+		}
+	}()
+	cr := &ClassReader(classData)
+	cf = &ClassFile{}
+	cf.read(cr)
+	return
 }
 
 func (self *ClassFile) read(reader *ClassReader) {
-
+	
 }
 
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
-
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic!")
+	}
 }
 
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
-
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion {
+		case 45:
+			return
+		case 46, 47, 48, 49, 50, 51, 52:
+			if self.minorVersion == 0 {
+				return
+			}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
 }
 
 func (self *ClassFile) MinorVersion() uint16 {
