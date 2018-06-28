2e44c9f1aee4c1feee960370e2b4e744ff503613
zhouyongfa <fightzyf1992@hotmail.com>
2018-06-28 19:40:07 +0800

    post constant info

--- ch03/classfile/class_file.go
+++ ch03/classfile/class_file.go
@@ -47,12 +47,12 @@ func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
 	self.minorVersion = reader.readUint16()
 	self.majorVersion = reader.readUint16()
 	switch self.majorVersion {
-		case 45:
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
 			return
-		case 46, 47, 48, 49, 50, 51, 52:
-			if self.minorVersion == 0 {
-				return
-			}
+		}
 	}
 	panic("java.lang.UnsupportedClassVersionError!")
 }
--- ch03/classfile/constant_pool.go
+++ ch03/classfile/constant_pool.go
@@ -1,5 +1,36 @@
 package classfile
 
+type ConstantInfo interface {
+	readInfo(reader *ClassReaser)
+}
+
+func readConstantInfo(reader *ClassReader, cp ConstantPool) ConstantInfo {
+	tag := reader.readUint8()
+	c := newConstantInfo(tag, cp)
+	c.readInfo(reader)
+	return c
+}
+
+func newConstantInfo(tag uint8, cp ConstantPool) ConstantInfo {
+	switch tag {
+	case CONSTANT_Integer: return &ConstantIntegerInfo{}
+	case CONSTANT_Float: return &ConstantFloatInfo{}
+	case CONSTANT_Long: return &ConstantLonginfo{}
+	case CONSTANT_Double: return &ConstantDoubleInfo{}
+	case CONSTANT_Utf8: return &ConstantUtf8Info{}
+	case CONSTANT_String: return &ConstantClassInfo{cp: cp}
+	case CONSTANT_Class: return &ConstantClassinfo{cp: cp}
+	case CONSTANT_Fieldref: return &ConstantFieldrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_Methodref: return &ConstantMethodrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_InterfaceMethodref: return &ConstantInterfaceMethodrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_NameAndType: return &ConstantNameAndTypeInfo{}
+	case CONSTANT_MethodType: return &ConstantMethodTypeInfo{}
+	case CONSTANT_MethodHandle: return &ConstantMethodHandleInfo{}
+	case CONSTANT_InvokeDynamic: return &ConstantInvokeDynamicInfo{}
+	default: panic("java.lang.ClassFormatError: constant pool tag!")
+	}
+}
+
 type ConstantPool [] ConstantInfo
 
 func readConstantPool(reader *ClassReader) ConstantPool {
