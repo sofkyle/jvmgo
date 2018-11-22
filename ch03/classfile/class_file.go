61b5a71d6f2d71ef998124316375377ab9351f4d
zhouyongfa <fightzyf1992@hotmail.com>
2018-11-22 16:57:04 +0800

    fix bug

--- ch03/classfile/class_file.go
+++ ch03/classfile/class_file.go
@@ -2,21 +2,41 @@ package classfile
 
 import "fmt"
 
+/*
+ClassFile {
+    u4             magic;
+    u2             minor_version;
+    u2             major_version;
+    u2             constant_pool_count;
+    cp_info        constant_pool[constant_pool_count-1];
+    u2             access_flags;
+    u2             this_class;
+    u2             super_class;
+    u2             interfaces_count;
+    u2             interfaces[interfaces_count];
+    u2             fields_count;
+    field_info     fields[fields_count];
+    u2             methods_count;
+    method_info    methods[methods_count];
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
 type ClassFile struct {
-	magic				uint32
-	minorVersion		uint16
-	majorVersion 		uint16
-	constantPool		ConstantPool
-	accessFlags			uint16 	// 类访问标志
-	thisClass			uint16	// 类名索引
-	superClass			uint16	// 父类名索引
-	interfaces			uint16	// 接口索引
-	fields				[] uint16	// 字段和方法索引表
-	methods				[] *MemberInfo
-	attributes			[] AttributeInfo
+	//magic      uint32
+	minorVersion uint16
+	majorVersion uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
+	fields       []*MemberInfo
+	methods      []*MemberInfo
+	attributes   []AttributeInfo
 }
 
-func Parse(classData [] byte) (cf *ClassFile, err error) {
+func Parse(classData []byte) (cf *ClassFile, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			var ok bool
@@ -34,7 +54,16 @@ func Parse(classData [] byte) (cf *ClassFile, err error) {
 }
 
 func (self *ClassFile) read(reader *ClassReader) {
-	
+	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+	self.constantPool = readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.superClass = reader.readUint16()
+	self.interfaces = reader.readUint16s()
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
 }
 
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
@@ -55,41 +84,44 @@ func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
 			return
 		}
 	}
+
 	panic("java.lang.UnsupportedClassVersionError!")
 }
 
 func (self *ClassFile) MinorVersion() uint16 {
-
+	return self.minorVersion
 }
-
 func (self *ClassFile) MajorVersion() uint16 {
-
+	return self.majorVersion
 }
-
 func (self *ClassFile) ConstantPool() ConstantPool {
-
+	return self.constantPool
 }
-
 func (self *ClassFile) AccessFlags() uint16 {
-
+	return self.accessFlags
 }
-
-func (self *ClassFile) Fields() [] *MemberInfo {
-
+func (self *ClassFile) Fields() []*MemberInfo {
+	return self.fields
 }
-
-func (self *ClassFile) Methods() [] *Memberinfo {
-
+func (self *ClassFile) Methods() []*MemberInfo {
+	return self.methods
 }
 
 func (self *ClassFile) ClassName() string {
-
+	return self.constantPool.getClassName(self.thisClass)
 }
 
 func (self *ClassFile) SuperClassName() string {
-
+	if self.superClass > 0 {
+		return self.constantPool.getClassName(self.superClass)
+	}
+	return ""
 }
 
-func (self *ClassFile) InterfaceNames() [] string {
-
+func (self *ClassFile) InterfaceNames() []string {
+	interfaceNames := make([]string, len(self.interfaces))
+	for i, cpIndex := range self.interfaces {
+		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
+	}
+	return interfaceNames
 }
--- ch03/classfile/cp_numeric.go
+++ ch03/classfile/cp_numeric.go
@@ -3,7 +3,6 @@ package classfile
 import "math"
 
 /*
- * Integer常量结构
 CONSTANT_Integer_info {
     u1 tag;
     u4 bytes;
@@ -17,9 +16,11 @@ func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint32()
 	self.val = int32(bytes)
 }
+func (self *ConstantIntegerInfo) Value() int32 {
+	return self.val
+}
 
 /*
- * Float常量结构
 CONSTANT_Float_info {
     u1 tag;
     u4 bytes;
@@ -33,9 +34,11 @@ func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint32()
 	self.val = math.Float32frombits(bytes)
 }
+func (self *ConstantFloatInfo) Value() float32 {
+	return self.val
+}
 
 /*
- * Long常量结构
 CONSTANT_Long_info {
     u1 tag;
     u4 high_bytes;
@@ -50,9 +53,11 @@ func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint64()
 	self.val = int64(bytes)
 }
+func (self *ConstantLongInfo) Value() int64 {
+	return self.val
+}
 
 /*
- * Double常量结构
 CONSTANT_Double_info {
     u1 tag;
     u4 high_bytes;
@@ -63,7 +68,10 @@ type ConstantDoubleInfo struct {
 	val float64
 }
 
-func (self *ConstantDoubleInfo) readerInfo(reader *ClassReader) {
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint64()
 	self.val = math.Float64frombits(bytes)
+}
+func (self *ConstantDoubleInfo) Value() float64 {
+	return self.val
 }
