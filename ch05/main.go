531115d53c2fb6133565fc82ae85f2c622a6e050
zhouyongfa <fightzyf1992@hotmail.com>
2019-02-20 21:15:33 +0800

    test chapter 5

--- ch05/main.go
+++ ch05/main.go
@@ -1,52 +1,53 @@
 package main
 
-//import "fmt"
-import _ "jvmgo/ch05/classfile"
-import _ "jvmgo/ch05/classpath"
-import "jvmgo/ch05/rtda"
+import "fmt"
+import "strings"
+import "jvmgo/ch05/classfile"
+import "jvmgo/ch05/classpath"
 
 func main() {
-	frame := rtda.NewFrame(100, 100)
-	testLocalVars(frame.LocalVars())
-	testOperandStack(frame.OperandStack())
+	cmd := parseCmd()
+
+	if cmd.versionFlag {
+		fmt.Println("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
 }
 
 func startJVM(cmd *Cmd) {
-	frame := rtda.NewFrame(100, 100)
-	testLocalVars(frame.LocalVars())
-	testOperandStack(frame.OperandStack())
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	mainMethod := getMainMethod(cf)
+	if mainMethod != nil {
+		interpret(mainMethod)
+	} else {
+		fmt.Printf("Main method not found in class %s\n", cmd.class)
+	}
 }
 
-func testLocalVars(vars rtda.LocalVars) {
-	vars.SetInt(0, 100)
-	vars.SetInt(1, -100)
-	vars.SetLong(2, 2997924580)
-	vars.SetLong(4, -2997924580)
-	vars.SetFloat(6, 3.1415926)
-	vars.SetDouble(7, 2.71828182845)
-	vars.SetRef(9, nil)
-	println(vars.GetInt(0))
-	println(vars.GetInt(1))
-	println(vars.GetLong(2))
-	println(vars.GetLong(4))
-	println(vars.GetFloat(6))
-	println(vars.GetDouble(7))
-	println(vars.GetRef(9))
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		panic(err)
+	}
+
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+
+	return cf
 }
 
-func testOperandStack(ops *rtda.OperandStack) {
-	ops.PushInt(100)
-	ops.PushInt(-100)
-	ops.PushLong(2997924580)
-	ops.PushLong(-2997924580)
-	ops.PushFloat(3.1415926)
-	ops.PushDouble(2.71828182845)
-	ops.PushRef(nil)
-	println(ops.PopRef())
-	println(ops.PopDouble())
-	println(ops.PopFloat())
-	println(ops.PopLong())
-	println(ops.PopLong())
-	println(ops.PopInt())
-	println(ops.PopInt())
+func getMainMethod(cf *classfile.ClassFile) *classfile.MemberInfo {
+	for _, m := range cf.Methods() {
+		if m.Name() == "main" && m.Descriptor() == "([Ljava/lang/String;)V" {
+			return m
+		}
+	}
+	return nil
 }
